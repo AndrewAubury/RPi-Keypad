b1eadcfa2a37480156a0bee3aa7fccd2ea006c29
Andrew Andrew <Andrew Andrew>
2016-03-21 20:39:44 +0000

    Fixed Some Bugs

--- src/me/Andrew/Keypad/Config/Settings.java
+++ src/me/Andrew/Keypad/Config/Settings.java
@@ -7,15 +7,15 @@ public class Settings {
 public String Code = "ABCD";
 
 //Define Row Pins!
-public Pin Row1 = RaspiPin.GPIO_00;
-public Pin Row2 = RaspiPin.GPIO_01;
-public Pin Row3 = RaspiPin.GPIO_02;
-public Pin Row4 = RaspiPin.GPIO_03;
+public Pin Row1 = RaspiPin.GPIO_04;
+public Pin Row2 = RaspiPin.GPIO_05;
+public Pin Row3 = RaspiPin.GPIO_06;
+public Pin Row4 = RaspiPin.GPIO_00;
 
 //Define Col Pins!
-public Pin Col1 = RaspiPin.GPIO_04;
-public Pin Col2 = RaspiPin.GPIO_05;
-public Pin Col3 = RaspiPin.GPIO_06;
-public Pin Col4 = RaspiPin.GPIO_07;
+public Pin Col1 = RaspiPin.GPIO_02;
+public Pin Col2 = RaspiPin.GPIO_03;
+public Pin Col3 = RaspiPin.GPIO_15;
+public Pin Col4 = RaspiPin.GPIO_16;
 
 }
